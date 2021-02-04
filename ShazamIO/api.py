18ca084b956dc94080cdee01ba0c608dc84f0d15
dotX12 <hash@shitposting.team>
2021-02-04 19:34:06 +0300

    🔧 Transfer funcs to converter

--- ShazamIO/api.py
+++ ShazamIO/api.py
@@ -8,16 +8,16 @@ import time
 from .algorithm import SignatureGenerator
 from .signature import DecodedMessage
 from .models import Request, ShazamUrl
+from .converter import Converter
 
 
-class Shazam:
+class Shazam(Converter):
     def __init__(self, song_data: bytes):
         self.songData = song_data
         self.audio = self.normalize_audio_data(self.songData)
-        self.MAX_TIME_SECONDS = 8
 
     async def recognize_song(self) -> dict:
-        signature_generator = self.createSignatureGenerator(self.audio)
+        signature_generator = self.create_signature_generator(self.audio)
         while True:
             signature = signature_generator.get_next_signature()
             if not signature:
@@ -42,21 +42,3 @@ class Shazam:
                         str(uuid.uuid4()).upper()),
                     headers=Request.HEADERS, json=data) as resp:
                 return await resp.json()
-
-    @staticmethod
-    def normalize_audio_data(song_data: bytes) -> AudioSegment:
-
-        audio = AudioSegment.from_file(BytesIO(song_data))
-        audio = audio.set_sample_width(2)
-        audio = audio.set_frame_rate(16000)
-        audio = audio.set_channels(1)
-
-        return audio
-
-    def createSignatureGenerator(self, audio: AudioSegment) -> SignatureGenerator:
-        signature_generator = SignatureGenerator()
-        signature_generator.feed_input(audio.get_array_of_samples())
-        signature_generator.MAX_TIME_SECONDS = self.MAX_TIME_SECONDS
-        if audio.duration_seconds > 12 * 3:
-            signature_generator.samples_processed += 16000 * (int(audio.duration_seconds / 16) - 6)
-        return signature_generator
--- ShazamIO/converter.py
+++ ShazamIO/converter.py
@@ -1,7 +1,11 @@
+from io import BytesIO
+
+from pydub import AudioSegment
+
+from ShazamIO.algorithm import SignatureGenerator
 from ShazamIO.typehints import *
 from ShazamIO.models import *
 import aiohttp
-
 
 class Converter(Request):
 
@@ -18,4 +22,24 @@ class Converter(Request):
                 for k, v in data.items():
                     if v == city:
                         return k.split('_')[2]
+
+    @staticmethod
+    def normalize_audio_data(song_data: bytes) -> AudioSegment:
+
+        audio = AudioSegment.from_file(BytesIO(song_data))
+        audio = audio.set_sample_width(2)
+        audio = audio.set_frame_rate(16000)
+        audio = audio.set_channels(1)
+
+        return audio
+
+    @staticmethod
+    def create_signature_generator(audio: AudioSegment) -> SignatureGenerator:
+        signature_generator = SignatureGenerator()
+        signature_generator.feed_input(audio.get_array_of_samples())
+        signature_generator.MAX_TIME_SECONDS = 8
+        if audio.duration_seconds > 12 * 3:
+            signature_generator.samples_processed += 16000 * (int(audio.duration_seconds / 16) - 6)
+        return signature_generator
+
 
