a44991b640ea6d3d597eb8605d718c66e8e23153
dotX12 <hash@shitposting.team>
2021-02-06 00:33:40 +0300

    🧑‍🎤 Added ArtistInfoNormalized, type hints

--- ShazamIO/converter.py
+++ ShazamIO/converter.py
@@ -10,23 +10,23 @@ from ShazamIO.models import *
 
 class TrackInfoNormalized:
     def __init__(self, track: dict):
-        self.track = track['track'] if 'timezone' in track else track
-        self.track_id = self.track['key']
-        self.title = self.track['title']
-        self.author = self.track['subtitle']
-        self.photo_url = self.track['images']['coverarthq'] if 'images' in self.track else None
-        self.ringtone_url = self.track['hub']['actions'][1]['uri'] if 'actions' in self.track['hub'] else None
-        self.artist_id = self.track['artists'][0]['id'] if 'artists' in self.track else None
-        self.shazam_url = f'https://www.shazam.com/track/{self.track_id}'
-        self.apple_music_url = self.track['hub']['options'][0]['actions'][0]['uri'].split("?i")[0]
-        self.providers = self.track['hub']['providers'] if 'providers' in self.track['hub'] else None
-        self.spotify_url = (self.track['hub']['providers'][0]['actions'][0]['uri']
-                            if self.providers is not None and
-                            self.track['hub']['providers'][0]['actions'][0]['uri'][:8] == 'https://' else None)
-        self.spotify_uri = (self.track['hub']['providers'][0]['actions'][1]['uri']
-                            if self.providers is not None and len(self.providers) > 1 else None)
-        self.shazam_uri_decoded = (self.spotify_uri.split('spotify:search:')[1]
-                                   if self.spotify_uri is not None else None)
+        self.track: dict = track['track'] if 'timezone' in track else track
+        self.track_id: int = self.track['key']
+        self.title: str = self.track['title']
+        self.author: str = self.track['subtitle']
+        self.photo_url: str = self.track['images']['coverarthq'] if 'images' in self.track else None
+        self.ringtone_url: str = self.track['hub']['actions'][1]['uri'] if 'actions' in self.track['hub'] else None
+        self.artist_id: int = self.track['artists'][0]['id'] if 'artists' in self.track else None
+        self.shazam_url: str = f'https://www.shazam.com/track/{self.track_id}'
+        self.apple_music_url: str = self.track['hub']['options'][0]['actions'][0]['uri'].split("?i")[0]
+        self.providers: str = self.track['hub']['providers'] if 'providers' in self.track['hub'] else None
+        self.spotify_url: str = (self.track['hub']['providers'][0]['actions'][0]['uri']
+                                 if self.providers is not None and
+                                 self.track['hub']['providers'][0]['actions'][0]['uri'][:8] == 'https://' else None)
+        self.spotify_uri: str = (self.track['hub']['providers'][0]['actions'][1]['uri']
+                                 if self.providers is not None and len(self.providers) > 1 else None)
+        self.shazam_uri_decoded: str = (self.spotify_uri.split('spotify:search:')[1]
+                                        if self.spotify_uri is not None else None)
 
     def __str__(self):
         return (f'Author: {self.author} - {self.title}\n'
@@ -34,11 +34,32 @@ class TrackInfoNormalized:
                 f'Shazam URL: {self.shazam_url}\n'
                 f'Spotify URL: {self.spotify_url}\n'
                 f'AppleMusic: {self.apple_music_url}\n'
-                
+
                 f'Ringtone: {self.ringtone_url}\n'
                 f'Track photo: {self.photo_url}\n'
                 f'Shazam URI: {self.spotify_uri}\n'
                 f'Short URI: {self.shazam_uri_decoded}\n')
+
+
+class ArtistInfoNormalized:
+    def __init__(self, artist: dict):
+        self.artist: dict = artist
+        self.name: str = self.artist['name']
+        self.alias: str = self.artist['alias']
+        self.verified: str = self.artist['verified']
+        self.genre_primary: str = self.artist['genres']['primary']
+        self.genres: list = self.artist['genres']['secondaries']
+        self.avatar: str = self.artist['avatar']
+        self.url: str = self.artist['weburl']
+
+    def __str__(self):
+        return (f'Name: {self.name}\n'
+                f'Alias: {self.alias}\n'
+                f'Verified: {self.verified}\n'
+                f'Primary genre: {self.genre_primary}\n'
+                f'All genres: {self.genres}\n'
+                f'Url: {self.url}\n'
+                f'Avatar: {self.avatar}\n')
 
 
 class Geo(HTTPClient):
