55b3026e15c0db1b826f84ceb7d20422c8ce21ae
dotX12 <hash@shitposting.team>
2021-02-05 17:18:58 +0300

    📝 New URL link

--- ShazamIO/models.py
+++ ShazamIO/models.py
@@ -2,25 +2,25 @@ from enum import IntEnum
 
 
 class ShazamUrl:
-    SEARCH_FROM_FILE = 'https://amp.shazam.com/discovery/v5/ru/RU/iphone/-/tag/{}/{}?sync=true&webv3=true' \
+    SEARCH_FROM_FILE = 'https://amp.shazam.com/discovery/v5/ru/GB/iphone/-/tag/{}/{}?sync=true&webv3=true' \
                        '&sampling=true&connected=&shazamapiversion=v3&sharehub=true&hubv5minorversion=v5.1&hidelb' \
                        '=true&video=v3'
-    TOP_TRACKS_WORLD = 'https://www.shazam.com/shazam/v3/en-US/PL/web/-/tracks/ip-global-chart?pageSize={}&startFrom={}'
+    TOP_TRACKS_WORLD = 'https://www.shazam.com/shazam/v3/en-US/GB/web/-/tracks/ip-global-chart?pageSize={}&startFrom={}'
     ARTIST_ABOUT = 'https://www.shazam.com/discovery/v3/ru/GB/web/artist/{}?shazamapiversion=v3&video=v3'
     ARTIST_TOP_TRACKS = 'https://cdn.shazam.com/shazam/v3/ru/GB/web/-/tracks/artisttoptracks_{}' \
                         '?startFrom={}&pageSize={}&connected=&channel='
     ABOUT_TRACK = 'https://www.shazam.com/discovery/v5/ru/PL/web/-/track/{}?shazamapiversion=v3&video=v3'
-    TOP_TRACKS_COUNTRY = 'https://www.shazam.com/shazam/v3/ru/PL/web/-/tracks/ip-country-chart-{}?pageSize={}' \
+    TOP_TRACKS_COUNTRY = 'https://www.shazam.com/shazam/v3/ru/GB/web/-/tracks/ip-country-chart-{}?pageSize={}' \
                          '&startFrom={}'
-    TOP_TRACKS_CITY = 'https://www.shazam.com/shazam/v3/ru/PL/web/-/tracks/ip-city-chart-{}?pageSize={}&' \
+    TOP_TRACKS_CITY = 'https://www.shazam.com/shazam/v3/en-US/PL/web/-/tracks/ip-city-chart-{}?pageSize={}&' \
                       'startFrom={}'
-    CITY_IDs_EN = 'https://www.shazam.com/builds/20210126-1f29a62a-655438/intl/cities/cities-en-US.json'
-    GENRE_WORLD = "https://www.shazam.com/shazam/v3/en-US/PL/web/-/tracks/genre-global-chart-{}" \
+    CITY_IDs = 'https://raw.githubusercontent.com/dotX12/dotX12/main/city.json'
+    GENRE_WORLD = "https://www.shazam.com/shazam/v3/en-US/GB/web/-/tracks/genre-global-chart-{}" \
                   "?pageSize={}&startFrom={}"
 
-    GENRE_COUNTRY = 'https://www.shazam.com/shazam/v3/en-US/PL/web/-/tracks/genre-country-chart-{}-{}?pageSize={}' \
+    GENRE_COUNTRY = 'https://www.shazam.com/shazam/v3/en-US/GB/web/-/tracks/genre-country-chart-{}-{}?pageSize={}' \
                     '&startFrom={}'
-    RELATED_SONGS = 'https://cdn.shazam.com/shazam/v3/ru/PL/web/-/tracks/track-similarities-id-{}?' \
+    RELATED_SONGS = 'https://cdn.shazam.com/shazam/v3/ru/GB/web/-/tracks/track-similarities-id-{}?' \
                     'startFrom={}&pageSize={}&connected=&channel=' # track, startFrom, pageSize
 
 class Request:
