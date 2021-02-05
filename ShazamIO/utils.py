f90858a49a6afcde7dd85839249e988bf1fe6f96
dotX12 <hash@shitposting.team>
2021-02-05 17:21:19 +0300

    📋 New argument content_type for converting json

--- ShazamIO/utils.py
+++ ShazamIO/utils.py
@@ -3,9 +3,9 @@ from aiohttp import ContentTypeError
 from ShazamIO.exceptions import FailedDecodeJson
 
 
-async def validate_json(resp: aiohttp.ClientResponse):
+async def validate_json(resp: aiohttp.ClientResponse, content_type: str = 'application/json') -> aiohttp.ClientResponse:
     try:
-        return await resp.json()
+        return await resp.json(content_type=content_type)
     except ContentTypeError as e:
         bad_url = str(str(e).split(",")[2]).split("'")[1]
         raise FailedDecodeJson(f"Check args, URL is invalid\nURL- {bad_url}")
