392efe4b5b0b0d4597ec6f3e3afedbfa6d68c1f7
dotX12 <hash@shitposting.team>
2021-02-06 00:19:18 +0300

    📝 Edited TrackInfoNormalized

--- ShazamIO/converter.py
+++ ShazamIO/converter.py
@@ -8,7 +8,7 @@ from ShazamIO.exceptions import BadCityName, BadCountryName
 from ShazamIO.models import *
 
 
-class AboutTrack:
+class TrackInfoNormalized:
     def __init__(self, track: dict):
         self.track = track['track'] if 'timezone' in track else track
         self.track_id = self.track['key']
@@ -18,13 +18,27 @@ class AboutTrack:
         self.ringtone_url = self.track['hub']['actions'][1]['uri'] if 'actions' in self.track['hub'] else None
         self.artist_id = self.track['artists'][0]['id'] if 'artists' in self.track else None
         self.shazam_url = f'https://www.shazam.com/track/{self.track_id}'
+        self.apple_music_url = self.track['hub']['options'][0]['actions'][0]['uri'].split("?i")[0]
+        self.providers = self.track['hub']['providers'] if 'providers' in self.track['hub'] else None
+        self.spotify_url = (self.track['hub']['providers'][0]['actions'][0]['uri']
+                            if self.providers is not None and
+                            self.track['hub']['providers'][0]['actions'][0]['uri'][:8] == 'https://' else None)
+        self.spotify_uri = (self.track['hub']['providers'][0]['actions'][1]['uri']
+                            if self.providers is not None and len(self.providers) > 1 else None)
+        self.shazam_uri_decoded = (self.spotify_uri.split('spotify:search:')[1]
+                                   if self.spotify_uri is not None else None)
 
     def __str__(self):
         return (f'Author: {self.author} - {self.title}\n'
-                f'Artist ID: {self.artist_id}'
+                f'Artist ID: {self.artist_id}\n'
                 f'Shazam URL: {self.shazam_url}\n'
+                f'Spotify URL: {self.spotify_url}\n'
+                f'AppleMusic: {self.apple_music_url}\n'
+                
                 f'Ringtone: {self.ringtone_url}\n'
-                f'Track photo: {self.photo_url}\n')
+                f'Track photo: {self.photo_url}\n'
+                f'Shazam URI: {self.spotify_uri}\n'
+                f'Short URI: {self.shazam_uri_decoded}\n')
 
 
 class Geo(HTTPClient):
@@ -58,7 +72,6 @@ class Converter:
 
     @staticmethod
     def normalize_audio_data(song_data: bytes) -> AudioSegment:
-
         audio = AudioSegment.from_file(BytesIO(song_data))
         audio = audio.set_sample_width(2)
         audio = audio.set_frame_rate(16000)
