9eb710fe2b24a60a9f561c10e51c1b08575e3cdd
dotX12 <hash@shitposting.team>
2021-02-07 02:38:26 +0300

    🛠 Add API methods (related_tracks, search_artist, search_track)

--- ShazamIO/api.py
+++ ShazamIO/api.py
@@ -5,52 +5,64 @@ import time
 import typing
 
 from .signature import DecodedMessage
-from .models import Request, ShazamUrl
+from .models import Request, ShazamUrl, GenreMusic
 from .converter import Converter
 from .client import HTTPClient
+from .utils import load_file
 
 
 class Shazam(Converter, HTTPClient):
 
-    async def top_world_tracks(self, tracks: int = 200, start_from: int = 0) -> dict:
-        return await self.request('GET', ShazamUrl.TOP_TRACKS_WORLD.format(tracks, start_from), headers=Request.HEADERS)
+    async def top_world_tracks(self, limit: int = 200, start_from: int = 0) -> dict:
+        return await self.request('GET', ShazamUrl.TOP_TRACKS_WORLD.format(limit, start_from), headers=Request.HEADERS)
 
     async def artist_about(self, artist_id: int):
         return await self.request('GET', ShazamUrl.ARTIST_ABOUT.format(artist_id))
 
-    async def artist_top_tracks(self, artist_id: int, tracks: int = 200, start_from: int = 200) -> dict:
-        return await self.request('GET', ShazamUrl.ARTIST_TOP_TRACKS.format(artist_id, start_from, tracks),
+    async def artist_top_tracks(self, artist_id: int, limit: int = 200, start_from: int = 0) -> dict:
+        return await self.request('GET', ShazamUrl.ARTIST_TOP_TRACKS.format(artist_id, start_from, limit),
                                   headers=Request.HEADERS)
 
     async def track_about(self, track_id: int) -> dict:
         return await self.request('GET', ShazamUrl.ABOUT_TRACK.format(track_id), headers=Request.HEADERS)
 
-    async def top_country_tracks(self, country: str, tracks: int = 200, start_from: int = 0):
-        return await self.request('GET', ShazamUrl.TOP_TRACKS_COUNTRY.format(country, tracks, start_from),
+    async def top_country_tracks(self, country: str, limit: int = 200, start_from: int = 0):
+        return await self.request('GET', ShazamUrl.TOP_TRACKS_COUNTRY.format(country, limit, start_from),
                                   headers=Request.HEADERS)
 
-    async def top_city_tracks(self, city_id: int, tracks: int = 200, start_from: int = 0) -> dict:
-        return await self.request('GET', ShazamUrl.TOP_TRACKS_CITY.format(city_id, tracks, start_from),
+    async def top_city_tracks(self, city_id: int, limit: int = 200, start_from: int = 0) -> dict:
+        return await self.request('GET', ShazamUrl.TOP_TRACKS_CITY.format(city_id, limit, start_from),
                                   headers=Request.HEADERS)
 
-    async def top_world_genre_tracks(self, genre: str, tracks: int = 200, start_from: int = 200) -> dict:
-        return await self.request('GET', ShazamUrl.GENRE_WORLD.format(genre, tracks, start_from),
+    async def top_world_genre_tracks(self, genre: typing.Union[GenreMusic, int], limit: int = 100,
+                                     start_from: int = 0) -> dict:
+        return await self.request('GET', ShazamUrl.GENRE_WORLD.format(genre, limit, start_from),
                                   headers=Request.HEADERS)
 
-    async def top_country_genre_tracks(self, country: str, genre: int, tracks: int = 200, start_from: int = 0):
-        return await self.request('GET', ShazamUrl.GENRE_COUNTRY.format(country, genre, tracks, start_from),
+    async def top_country_genre_tracks(self, country: str, genre: typing.Union[GenreMusic, int], limit: int = 200,
+                                       start_from: int = 0) -> dict:
+        return await self.request('GET', ShazamUrl.GENRE_COUNTRY.format(country, genre, limit, start_from),
                                   headers=Request.HEADERS)
 
-    async def recognize_song(self, path_file: typing.Union[str, pathlib.Path]) -> dict:
+    async def related_tracks(self, track_id: int, limit: int = 20, start_from: int = 0) -> dict:
+        return await self.request('GET', ShazamUrl.RELATED_SONGS.format(track_id, start_from, limit),
+                                  headers=Request.HEADERS)
 
-        with open(path_file, 'rb') as file:
-            audio = self.normalize_audio_data(file.read())
-            signature_generator = self.create_signature_generator(audio)
+    async def search_artist(self, query: str, limit: int = 10) -> dict:
+        return await self.request('GET', ShazamUrl.SEARCH_ARTIST.format(query, limit), headers=Request.HEADERS)
+
+    async def search_track(self, query: str, limit: int = 10) -> dict:
+        return await self.request('GET', ShazamUrl.SEARCH_MUSIC.format(query, limit), headers=Request.HEADERS)
+
+    async def recognize_song(self, file_path: typing.Union[str, pathlib.Path]) -> dict:
+        file = await load_file(file_path, 'rb')
+        audio = await self.normalize_audio_data(file)
+        signature_generator = await self.create_signature_generator(audio)
+        signature = await signature_generator.get_next_signature()
+        while not signature:
             signature = signature_generator.get_next_signature()
-            while not signature:
-                signature = signature_generator.get_next_signature()
-            results = await self.send_recognize_request(signature)
-            return results
+        results = await self.send_recognize_request(signature)
+        return results
 
     async def send_recognize_request(self, sig: DecodedMessage) -> dict:
         data = Converter.data_search(Request.TIME_ZONE, sig.encode_to_uri(),
