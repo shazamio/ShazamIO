a83c8f03bfb479a9532f2175f94698271332f00d
dotX12 <hash@shitposting.team>
2021-02-07 02:34:27 +0300

    🛠 Add API methods (related_tracks, search_artist, search_track)

--- ShazamIO/models.py
+++ ShazamIO/models.py
@@ -2,26 +2,25 @@ from enum import IntEnum
 
 
 class ShazamUrl:
-    SEARCH_FROM_FILE = 'https://amp.shazam.com/discovery/v5/ru/GB/iphone/-/tag/{}/{}?sync=true&webv3=true' \
+    SEARCH_FROM_FILE = 'https://amp.shazam.com/discovery/v5/en/GB/iphone/-/tag/{}/{}?sync=true&webv3=true' \
                        '&sampling=true&connected=&shazamapiversion=v3&sharehub=true&hubv5minorversion=v5.1&hidelb' \
-                       '=true&video=v3'
-    TOP_TRACKS_WORLD = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-global-chart?pageSize={}&startFrom={}'
-    ARTIST_ABOUT = 'https://www.shazam.com/discovery/v3/en/GB/web/artist/{}?shazamapiversion=v3&video=v3'
-    ARTIST_TOP_TRACKS = 'https://cdn.shazam.com/shazam/v3/en/GB/web/-/tracks/artisttoptracks_{}' \
-                        '?startFrom={}&pageSize={}&connected=&channel='
-    ABOUT_TRACK = 'https://www.shazam.com/discovery/v5/en/GB/web/-/track/{}?shazamapiversion=v3&video=v3'
+                       '=true&video=v3'  # TESTED
+    TOP_TRACKS_WORLD = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-global-chart?pageSize={}&startFrom={}'  # TESTED
+    ARTIST_ABOUT = 'https://www.shazam.com/discovery/v3/en/GB/web/artist/{}?shazamapiversion=v3&video=v3'  # TESTED
+    ARTIST_TOP_TRACKS = 'https://cdn.shazam.com/shazam/v3/en/GB/web/-/tracks/artisttoptracks_{}?startFrom={}' \
+                        '&pageSize={}&connected=&channel='  # TESTED
+    ABOUT_TRACK = 'https://www.shazam.com/discovery/v5/en/GB/web/-/track/{}?shazamapiversion=v3&video=v3'  # TESTED
     TOP_TRACKS_COUNTRY = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-country-chart-{}?pageSize={}' \
-                         '&startFrom={}'
+                         '&startFrom={}'  # TESTED
     TOP_TRACKS_CITY = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-city-chart-{}?pageSize={}&' \
-                      'startFrom={}'
-    CITY_IDs = 'https://raw.githubusercontent.com/dotX12/dotX12/main/city.json'
+                      'startFrom={}'  # TESTED
+    CITY_IDS = 'https://raw.githubusercontent.com/dotX12/dotX12/main/city.json'
     GENRE_WORLD = "https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/genre-global-chart-{}" \
-                  "?pageSize={}&startFrom={}"
-
+                  "?pageSize={}&startFrom={}"  # TESTED
     GENRE_COUNTRY = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/genre-country-chart-{}-{}?pageSize={}' \
-                    '&startFrom={}'
-    RELATED_SONGS = 'https://cdn.shazam.com/shazam/v3/en/GB/web/-/tracks/track-similarities-id-{}?' \
-                    'startFrom={}&pageSize={}&connected=&channel='
+                    '&startFrom={}'  # TESTED
+    RELATED_SONGS = 'https://cdn.shazam.com/shazam/v3/en/GB/web/-/tracks/track-similarities-id-{}' \
+                    '?startFrom={}&pageSize={}&connected=&channel='  # TESTED
     SEARCH_ARTIST = 'https://www.shazam.com/services/search/v3/en/GB/web/search?query={}' \
                     '&numResults={}&offset=0&types=artists'  # запрос, количество.
 
@@ -43,28 +42,28 @@ class Request:
 
 
 class GenreMusic(IntEnum):
-    Pop = 1
-    Hip_Hop_Rap = 2
-    Dance = 3
-    Electronic = 4
-    RNB_Soul = 5
-    Alternative = 6
-    Rock = 7
-    Latin = 8
-    Film_TV_Stage = 9
-    Country = 10
-    AfroBeats = 11
-    Worldwide = 12
-    Reggae_Dancehall = 13
-    House = 14
-    K_Pop = 15
-    French_Pop = 16
-    Singer_Songwriter = 17
-    Regional_Mexicano = 18
+    POP = 1
+    HIP_HOP_RAP = 2
+    DANCE = 3
+    ELECTRONIC = 4
+    RNB_SOUL = 5
+    ALTERNATIVE = 6
+    ROCK = 7
+    LATIN = 8
+    FILM_TV_STAGE = 9
+    COUNTRY = 10
+    AFRO_BEATS = 11
+    WORLDWIDE = 12
+    REGGAE_DANCE_HALL = 13
+    HOUSE = 14
+    K_POP = 15
+    FRENCH_POP = 16
+    SINGER_SONGWRITER = 17
+    REGIONAL_MEXICANO = 18
 
 
-class SampleRate(IntEnum):  # Enum keys are sample rates in Hz
-
+class SampleRate(IntEnum):
+    # Enum keys are sample rates in Hz
     _8000 = 1
     _11025 = 2
     _16000 = 3
@@ -73,8 +72,8 @@ class SampleRate(IntEnum):  # Enum keys are sample rates in Hz
     _48000 = 6
 
 
-class FrequencyBand(IntEnum):  # Enum keys are frequency ranges in Hz
-
+class FrequencyBand(IntEnum):
+    # Enum keys are frequency ranges in Hz
     hz_0_250 = -1  # Nothing above 250 Hz is actually stored
     hz_250_520 = 0
     hz_520_1450 = 1
