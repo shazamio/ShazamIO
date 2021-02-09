179f245ceb34ca557429156436c9dab550742713
dotX12 <hash@shitposting.team>
2021-02-09 03:04:36 +0300

    🏭 Update factory | dataclasses

--- ShazamIO/misc.py
+++ ShazamIO/misc.py
@@ -1,5 +1,5 @@
 from dataclass_factory import Factory
-from ShazamIO.factory import ArtistInfo, artist_info_schema
+from ShazamIO.factory import ArtistInfo, artist_info_schema, TrackInfo, track_info_schema
 
 factory_artist = Factory(schemas={ArtistInfo: artist_info_schema}, debug_path=True)
-
+factory_track = Factory(schemas={TrackInfo: track_info_schema}, debug_path=True)
