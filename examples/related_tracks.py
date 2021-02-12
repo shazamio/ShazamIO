15f27cb3352e87186e5e3d498e9cda6e89ccad40
dotX12 <hash@shitposting.team>
2021-02-12 04:30:41 +0300

    🧱 Example related tracks

---
+++ examples/related_tracks.py
@@ -0,0 +1,12 @@
+import asyncio
+from ShazamIO.api import Shazam
+
+
+async def main():
+    shazam = Shazam()
+    track_id = 546891609
+    related = await shazam.related_tracks(track_id=track_id, limit=5, start_from=2)  # ONLY №3, №4 SONG
+    print(related)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
