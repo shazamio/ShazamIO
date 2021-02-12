278d2c802fa29fa4399ca8deacfdcdfc48000923
dotX12 <hash@shitposting.team>
2021-02-12 04:13:10 +0300

    🧱 Example top tracks genre country

---
+++ examples/top_tracks_genre_country.py
@@ -0,0 +1,12 @@
+import asyncio
+from ShazamIO.api import Shazam
+
+
+async def main():
+    shazam = Shazam()
+    top_spain_rap = await shazam.top_country_genre_tracks(country='ES', genre=2, limit=4)
+    print(top_spain_rap)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
