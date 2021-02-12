44d7f20970efc0d8991941fa7e71d17bc0761796
dotX12 <hash@shitposting.team>
2021-02-12 04:37:55 +0300

    🧱 Example search tracks

---
+++ examples/search_tracks.py
@@ -0,0 +1,11 @@
+import asyncio
+from ShazamIO.api import Shazam
+
+
+async def main():
+    shazam = Shazam()
+    tracks = await shazam.search_track(query='Lil', limit=5)
+    print(tracks)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
