c3aec54db05d015a27a37a446c41ce2f9456806e
dotX12 <hash@shitposting.team>
2021-03-01 02:40:33 +0300

    🖍 Edit init

---
+++ ShazamIO/__init__.py
@@ -0,0 +1,8 @@
+from .factory import FactoryArtist, FactoryTrack
+from .api import Shazam
+from .converter import Geo
+
+__all__ = ('FactoryArtist',
+           'FactoryTrack',
+           'Shazam',
+           'Geo')
