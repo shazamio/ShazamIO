3cf4c45c6fde82fd590118591ac31f32f0feb985
dotX12 <hash@shitposting.team>
2021-03-01 03:08:38 +0300

    📝 Reformat API Links

--- ShazamIO/models.py
+++ ShazamIO/models.py
@@ -2,30 +2,34 @@ from enum import IntEnum
 
 
 class ShazamUrl:
-    SEARCH_FROM_FILE = 'https://amp.shazam.com/discovery/v5/en/GB/iphone/-/tag/{}/{}?sync=true&webv3=true' \
-                       '&sampling=true&connected=&shazamapiversion=v3&sharehub=true&hubv5minorversion=v5.1&hidelb' \
-                       '=true&video=v3'  # TESTED
-    TOP_TRACKS_WORLD = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-global-chart?pageSize={}&startFrom={}'  # TESTED
-    ARTIST_ABOUT = 'https://www.shazam.com/discovery/v3/en/GB/web/artist/{}?shazamapiversion=v3&video=v3'  # TESTED
-    ARTIST_TOP_TRACKS = 'https://cdn.shazam.com/shazam/v3/en/GB/web/-/tracks/artisttoptracks_{}?startFrom={}' \
-                        '&pageSize={}&connected=&channel='  # TESTED
-    ABOUT_TRACK = 'https://www.shazam.com/discovery/v5/en/GB/web/-/track/{}?shazamapiversion=v3&video=v3'  # TESTED
-    TOP_TRACKS_COUNTRY = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-country-chart-{}?pageSize={}' \
-                         '&startFrom={}'  # TESTED
-    TOP_TRACKS_CITY = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-city-chart-{}?pageSize={}&' \
-                      'startFrom={}'  # TESTED
+    SEARCH_FROM_FILE = (
+        'https://amp.shazam.com/discovery/v5/en/GB/iphone/-/tag/{}/{}?sync=true&webv3=true&sampling=true'
+        '&connected=&shazamapiversion=v3&sharehub=true&hubv5minorversion=v5.1&hidelb=true&video=v3')
+    TOP_TRACKS_WORLD = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-global-chart?pageSize={}&startFrom={}'
+    ARTIST_ABOUT = 'https://www.shazam.com/discovery/v3/en/GB/web/artist/{}?shazamapiversion=v3&video=v3'
+    ARTIST_TOP_TRACKS = (
+        'https://cdn.shazam.com/shazam/v3/en/GB/web/-/tracks/artisttoptracks_{}?startFrom={}'
+        '&pageSize={}&connected=&channel=')
+    ABOUT_TRACK = 'https://www.shazam.com/discovery/v5/en/GB/web/-/track/{}?shazamapiversion=v3&video=v3'
+    TOP_TRACKS_COUNTRY = (
+        'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-country-chart-{}?pageSize={}&startFrom={}')
+    TOP_TRACKS_CITY = (
+        'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/ip-city-chart-{}?pageSize={}&startFrom={}')
     CITY_IDS = 'https://raw.githubusercontent.com/dotX12/dotX12/main/city.json'
-    GENRE_WORLD = "https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/genre-global-chart-{}" \
-                  "?pageSize={}&startFrom={}"  # TESTED
-    GENRE_COUNTRY = 'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/genre-country-chart-{}-{}?pageSize={}' \
-                    '&startFrom={}'  # TESTED
-    RELATED_SONGS = 'https://cdn.shazam.com/shazam/v3/en/GB/web/-/tracks/track-similarities-id-{}' \
-                    '?startFrom={}&pageSize={}&connected=&channel='  # TESTED
-    SEARCH_ARTIST = 'https://www.shazam.com/services/search/v3/en/GB/web/search?query={}' \
-                    '&numResults={}&offset=0&types=artists'  # запрос, количество.
+    GENRE_WORLD = (
+        "https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/genre-global-chart-{}?pageSize={}&startFrom={}")
+    GENRE_COUNTRY = (
+        'https://www.shazam.com/shazam/v3/en/GB/web/-/tracks/genre-country-chart-{}-{}?pageSize={}&startFrom={}')
+    RELATED_SONGS = (
+        'https://cdn.shazam.com/shazam/v3/en/GB/web/-/tracks/track-similarities-id-{}'
+        '?startFrom={}&pageSize={}&connected=&channel=')
+    SEARCH_ARTIST = (
+        'https://www.shazam.com/services/search/v3/en/GB/web/search?query={}'
+        '&numResults={}&offset=0&types=artists')
 
-    SEARCH_MUSIC = 'https://www.shazam.com/services/search/v3/en/GB/web/search?query={}' \
-                    '&numResults={}&offset=0&types=songs'  # запрос, количество.
+    SEARCH_MUSIC = (
+        'https://www.shazam.com/services/search/v3/en/GB/web/search?query={}'
+        '&numResults={}&offset=0&types=songs')
 
 
 class Request:
