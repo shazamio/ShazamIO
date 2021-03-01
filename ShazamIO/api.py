0fda585206bd261daf6c1c9f15f82250bc9a57ef
dotX12 <hash@shitposting.team>
2021-03-01 07:00:29 +0300

    🌎 Moved city search to API

--- ShazamIO/api.py
+++ ShazamIO/api.py
@@ -6,12 +6,13 @@ import typing
 
 from .signature import DecodedMessage
 from .models import Request, ShazamUrl, GenreMusic
-from .converter import Converter
-from .client import HTTPClient
+from .converter import Converter, Geo
+from .typehints import CountryCode, ShazamResponse
 from .utils import load_file
 
 
-class Shazam(Converter, HTTPClient):
+class Shazam(Converter, Geo):
+    """Is a asynchronous framework for reverse engineered Shazam API written in Python 3.7 with asyncio and aiohttp."""
 
     async def top_world_tracks(self, limit: int = 200, start_from: int = 0) -> dict:
         return await self.request('GET', ShazamUrl.TOP_TRACKS_WORLD.format(limit, start_from), headers=Request.HEADERS)
@@ -30,8 +31,23 @@ class Shazam(Converter, HTTPClient):
         return await self.request('GET', ShazamUrl.TOP_TRACKS_COUNTRY.format(country, limit, start_from),
                                   headers=Request.HEADERS)
 
-    async def top_city_tracks(self, city_id: int, limit: int = 200, start_from: int = 0) -> dict:
-        return await self.request('GET', ShazamUrl.TOP_TRACKS_CITY.format(city_id, limit, start_from),
+    async def top_city_tracks(self, country_code: typing.Union[CountryCode, str],
+                              city_name: str, limit: int = 200,
+                              start_from: int = 0) -> typing.Union[ShazamResponse, dict]:
+
+        """
+            :param country_code: ISO 3166-3 alpha-2 code. Example: RU,NL,UA
+            :param city_name: City name from https://github.com/dotX12/dotX12/blob/main/city.json
+                Example: Budapest, Moscow
+            :param limit: Determines how many songs the maximum can be in the request.
+                Example: If 5 is specified, the query will return no more than 5 songs.
+            :param start_from: A parameter that determines with which song to display the request.
+                The default is 0. If you want to skip the first few songs, set this parameter to your own.
+            :return: dict songs
+        """
+        city_id = await Geo(country_code, city_name).city_id_from()
+        return await self.request('GET',
+                                  ShazamUrl.TOP_TRACKS_CITY.format(city_id, limit, start_from),
                                   headers=Request.HEADERS)
 
     async def top_world_genre_tracks(self, genre: typing.Union[GenreMusic, int], limit: int = 100,
@@ -69,6 +85,6 @@ class Shazam(Converter, HTTPClient):
                                      int(sig.number_samples / sig.sample_rate_hz * 1000), int(time.time() * 1000))
 
         return await self.request('POST', ShazamUrl.SEARCH_FROM_FILE.format(
-                                      str(uuid.uuid4()).upper(),
-                                      str(uuid.uuid4()).upper()),
+            str(uuid.uuid4()).upper(),
+            str(uuid.uuid4()).upper()),
                                   headers=Request.HEADERS, json=data)
--- examples/top_tracks_city.py
+++ examples/top_tracks_city.py
@@ -1,18 +1,16 @@
 import asyncio
-from ShazamIO import Shazam, FactoryTrack, Geo
+from ShazamIO import Shazam, FactoryTrack
 
 
 async def main():
     shazam = Shazam()
-    moscow_id = await Geo(country='RU', city='Moscow').city_id_from()
-    top_ten_moscow_tracks = await shazam.top_city_tracks(city_id=moscow_id, limit=10)
+    top_ten_moscow_tracks = await shazam.top_city_tracks(country_code='RU', city_name='Moscow', limit=10)
     print(top_ten_moscow_tracks)
     # ALL TRACKS DICT
     for track in top_ten_moscow_tracks['tracks']:
-        serialized = FactoryTrack(track)
+        serialized = FactoryTrack(track).serializer()
         # SERIALIZE FROM DATACLASS FACTORY
         print(serialized)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
-
