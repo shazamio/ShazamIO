9dd971ffaa898a9e02c68370fc2e9d6668c6c69c
dotX12 <hash@shitposting.team>
2021-03-01 09:49:45 +0300

    ✂ Del threaded

--- ShazamIO/utils.py
+++ ShazamIO/utils.py
@@ -15,14 +15,6 @@ async def validate_json(resp: aiohttp.ClientResponse, content_type: str = 'appli
         raise FailedDecodeJson(f"Check args, URL is invalid\nURL- {bad_url}")
 
 
-def threaded(func):
-    @wraps(func)
-    async def wrap(*args, **kwargs):
-        loop = asyncio.get_event_loop()
-        return await loop.run_in_executor(None, partial(func, *args, **kwargs))
-    return wrap
-
-
 async def load_file(file, binary=False):
     mode = "r" if not binary else "rb"
     async with aiofiles.open(file, mode=mode) as f:
