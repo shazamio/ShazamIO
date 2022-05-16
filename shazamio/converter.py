0baa29c3d6641511982144f83f9f84f5cdf28b8d
dotX12 <hash@shitposting.team>
2022-05-16 05:09:48 +0300

    Micro fixes

--- shazamio/converter.py
+++ shazamio/converter.py
@@ -57,7 +57,7 @@ class Converter:
     def create_signature_generator(audio: AudioSegment) -> SignatureGenerator:
         signature_generator = SignatureGenerator()
         signature_generator.feed_input(audio.get_array_of_samples())
-        signature_generator.MAX_TIME_SECONDS = 8
+        signature_generator.MAX_TIME_SECONDS = 12
         if audio.duration_seconds > 12 * 3:
-            signature_generator.samples_processed += 16000 * (int(audio.duration_seconds / 16) - 6)
+            signature_generator.samples_processed += 16000 * (int(audio.duration_seconds / 2) - 6)
         return signature_generator
