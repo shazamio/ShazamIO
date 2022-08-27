1e982bbdbd5928e7d2912820908f77893b2d0849
Andrew <dronablo@yandex.ru>
2022-08-27 21:33:37 +0300

    Fix hang when recognize_song feed with less than 8ms sound

--- tests/test_recognize.py
+++ tests/test_recognize.py
@@ -1,7 +1,10 @@
 import pytest_asyncio
+from pydub import AudioSegment
+from io import BytesIO
 
 from shazamio import Shazam
 from shazamio.utils import get_file_bytes
+
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -23,3 +26,17 @@ async def test_recognize_song_bytes(song_bytes: bytes):
 
     assert out.get("matches") != []
     assert out["track"]["key"] == "549679333"
+
+
+async def test_recognize_song_too_short():
+    short_audio_segment = AudioSegment.from_file(file=BytesIO(b'0'*126),
+                                                 format='pcm',
+                                                 sample_width=2,
+                                                 frame_rate=16000,
+                                                 channels=1)
+
+    shazam = Shazam()
+    out = await shazam.recognize_song(data=short_audio_segment)
+
+    assert out.get("matches") == []
+    assert "track" not in out
