ee77a374cff2fd33a2b2e1df7f4d6355311c17eb
autoblack <cclauss@users.noreply.github.com>
2023-12-10 21:30:51 +0000

    fixup: Format Python code with Black

--- tests/test_get_next_signature.py
+++ tests/test_get_next_signature.py
@@ -7,16 +7,12 @@ import pytest
 @pytest.fixture
 def signature_instance():
     instance = SignatureGenerator()
-    instance.input_pending_processing = [
-        random.randint(-32768, 32767) for _ in range(128 * 2)
-    ]
+    instance.input_pending_processing = [random.randint(-32768, 32767) for _ in range(128 * 2)]
     return instance
 
 
 def test_no_samples_to_process(signature_instance):
-    signature_instance.samples_processed = len(
-        signature_instance.input_pending_processing
-    )
+    signature_instance.samples_processed = len(signature_instance.input_pending_processing)
     result = signature_instance.get_next_signature()
     assert result is None
 
