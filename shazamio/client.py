9ff3e784a566b261d5aa160ba77c868e6a11ce0a
AveryanAlex <alex@averyan.ru>
2024-02-04 20:22:28 +0300

    Cache aiohttp.ClientSession

--- shazamio/client.py
+++ shazamio/client.py
@@ -5,14 +5,17 @@ from shazamio.utils import validate_json
 
 
 class HTTPClient:
-    @staticmethod
-    async def request(method: str, url: str, *args, **kwargs) -> dict:
-        async with aiohttp.ClientSession() as session:
-            if method.upper() == "GET":
-                async with session.get(url, **kwargs) as resp:
-                    return await validate_json(resp, *args)
-            elif method.upper() == "POST":
-                async with session.post(url, **kwargs) as resp:
-                    return await validate_json(resp, *args)
-            else:
-                raise BadMethod("Accept only GET/POST")
+    http_session: aiohttp.ClientSession
+
+    def __init__(self, *args, **kwargs):
+        self.http_session = aiohttp.ClientSession()
+
+    async def request(self, method: str, url: str, *args, **kwargs) -> dict:
+        if method.upper() == "GET":
+            async with self.http_session.get(url, **kwargs) as resp:
+                return await validate_json(resp, *args)
+        elif method.upper() == "POST":
+            async with self.http_session.post(url, **kwargs) as resp:
+                return await validate_json(resp, *args)
+        else:
+            raise BadMethod("Accept only GET/POST")
