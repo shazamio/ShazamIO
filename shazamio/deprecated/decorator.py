6020fdd1ba1b43fffc81535c04cfe8c454a01c67
dotX12 <dev@shitposting.team>
2024-02-17 23:40:45 +0300

    black code

--- shazamio/deprecated/decorator.py
+++ shazamio/deprecated/decorator.py
@@ -9,17 +9,13 @@ def deprecated(reason: str) -> Callable[[F], F]:
     def decorator(func: F) -> F:
         @functools.wraps(func)
         def new_func(*args: Any, **kwargs: Any) -> Any:
-            warnings.simplefilter(
-                "always", DeprecationWarning
-            )
+            warnings.simplefilter("always", DeprecationWarning)
             warnings.warn(
                 f"Call to deprecated function {func.__name__}. {reason}",
                 category=DeprecationWarning,
                 stacklevel=2,
             )
-            warnings.simplefilter(
-                "default", DeprecationWarning
-            )
+            warnings.simplefilter("default", DeprecationWarning)
             return func(*args, **kwargs)
 
         return cast(F, new_func)
