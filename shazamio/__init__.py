0f25db7a75a6a2548e47ddbc6206e007f9e8826a
dotX12 <dev@shitposting.team>
2024-02-25 04:04:28 +0300

    override max_timeout retry 60sec, and added logic for override retry condition.

--- shazamio/__init__.py
+++ shazamio/__init__.py
@@ -2,5 +2,6 @@ from .serializers import Serialize
 from .api import Shazam
 from .converter import GeoService
 from .enums import GenreMusic
+from .client import HTTPClient
 
-__all__ = ("Serialize", "Shazam", "GeoService", "GenreMusic")
+__all__ = ("Serialize", "Shazam", "GeoService", "GenreMusic", "HTTPClient")
